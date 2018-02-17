88baaab2621bc4ba69e2240c25da01094ce579a8
Nikita Sasunkevich <nikita@Nikitas-MacBook-Pro.local>
2018-02-17 00:12:55 -0700

    updated connection.js to use JAWSDB if available

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect((err) => {
     if(err){
